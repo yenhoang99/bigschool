ec985cba0321d5ba3e226a45434330f587431ed4
Administrator <Administrator@PC202>
2020-06-12 17:11:01 +0700

    Add project files.

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,54 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        // GET: Courses
+        public CoursesController()
+        {
+            _dbcontext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbcontext.Categories.ToList()
+            };
+
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbcontext.Categories.ToList();
+                return View("Index", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime  = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbcontext.Courses.Add(course);
+            _dbcontext.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+        
+        
+
+    }
+}
---
+++ BigSchool/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BigSchool.Models
+{
+    public class Category
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+    }
+}
